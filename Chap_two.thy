1acad11a1e91cb5d62ba852571cccf09b4d8c089
Ereshkigal <ereshkigal@irkalla.noreply>
2014-01-06 08:18:21 +0400

    2.7 in progress

--- Chap_two.thy
+++ Chap_two.thy
@@ -109,7 +109,26 @@ apply(induction)
 apply(auto)
 done
 
+(* 2.7 *)
 
+datatype 'a tree2 = Tip 'a | Node "'a tree2" 'a "'a tree2"
+
+fun mirror :: "'a tree2 \<Rightarrow> 'a tree2" where
+"mirror (Tip a) = Tip a" |
+"mirror (Node l a r) = Node (mirror r) a (mirror l)"
+
+fun pre_order :: "'a tree2 \<Rightarrow> 'a list" where
+"pre_order (Tip a) = [a]" |
+"pre_order (Node l a r) = a # (pre_order l) @ (pre_order r)"
+
+fun post_order :: "'a tree2 \<Rightarrow> 'a list" where
+"post_order (Tip a) = [a]" |
+"post_order (Node l a r) = (post_order l) @ (post_order r) @ [a]"
+
+lemma rev_order: "pre_order (mirror t) = rev (post_order t)"
+apply(induction t)
+apply(auto)
+done
 
 end
 
